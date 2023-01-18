988d1b78c8cffaf7f5cf1347c8c09e5567ccef10
luiluma <luma24lopez@gmail.com>
2023-01-17 20:40:12 -0500

    Binary insert left change error

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_insert_left - inserts a node as the left
- * 
+ *
  * @parent: pointer to the node insert a left
  * @value: value to store in the new node
  * Return: pointer to the created note or NULL on failure or if parent is NULL
@@ -15,11 +15,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (NULL);
-	
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->parent = parent;
 	new_node->n = value;
 	new_node->left = NULL;
@@ -27,7 +25,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
-		new_node->left = new_node;
+		new_node->left->parent = new_node;
 	}
 	parent->left = new_node;
 	return (new_node);
