df7dc0e8a11fd9ae751fe559b8eb1357883ba26a
CharlineMosquera <char951144@gmail.com>
2023-01-19 13:02:47 -0500

    binary tree uncle -18
    Co-authored-by: luiluma <luma24lopez@gmail.com>

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -4,18 +4,22 @@
  * binary_tree_uncle - function that finds the uncle of a node
  * @node: is a pointer to the node to find the uncle
  * Return: a pointer to the uncle node
-*/
+ */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    binary_tree_t *grandpa;
-    if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-        return (NULL);
+	binary_tree_t *grandpa;
 
-    grandpa = node->parent->parent;
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
 
-    if (grandpa->left == node->parent) {
-        return grandpa->right;
-    } else {
-        return grandpa->left;
-    }
+	grandpa = node->parent->parent;
+
+	if (grandpa->left == node->parent)
+	{
+		return (grandpa->right);
+	}
+	else
+	{
+		return (grandpa->left);
+	}
 }
